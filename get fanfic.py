fdb757e750191fa4c2dac1350bd6137caa246a45
thewonelolf <thewonelolf@github.com>
2020-02-21 16:49:36 -0500

    Basics are working - scraping and inserting fully functional, can detect if story already in DB

--- get fanfic.py
+++ get fanfic.py
@@ -1,13 +1,13 @@
-import time, os, fanfiction, requests, bs4, sqlite3
+import time, os, scraper, requests, bs4, sqlite3, traceback, codecs
 
 
-connection = sqlite3.connect("fanfiction.db")
+connection = sqlite3.connect('fanfiction.db')
 c = connection.cursor()
 
 c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='fanfiction' ''')
 
 if c.fetchone()[0]==0:
-    sql_command = ("""
+    sql_command = ('''
     CREATE TABLE fanfiction (
         id INTEGER PRIMARY KEY,
         canon_type TEXT,
@@ -24,22 +24,40 @@ if c.fetchone()[0]==0:
         reviews INTEGER,
         favs INTEGER,
         follows INTEGER,
-        status INTEGER
-    );""")
+        status INTEGER,
+        story TEXT
+    );''')
 
     c.execute(sql_command)
 else:
-    print("Table exists")
+    print('Table exists')
 
-baseURL = str("http://fanfiction.net/")
+baseURL = str('http://fanfiction.net/s/')
+ffscraper = scraper.Scraper()
+x = 0
 
-scraper = fanfiction.Scraper()
-metadata = scraper.scrape_story_metadata()
- 
-entry_str="""INSERT INTO fanfiction (id, canon_type, canon, authorid, title, updated, published, language, genre, rating, chapters, words, reviews, favs, follows, status)
-            VALUES ("{id}", "{canon_type}", "{canon}", "{authorid}", "{title}", "{updated}", "{published}", "{language}", "{genre}", "{rating}", "{chapters}", "{words}", "{reviews}", "{favs}", "{follows}", "{status}");"""
-print(int(metadata.get('id')))
-entry_command = entry_str.format(id = metadata.get('id'), canon_type = metadata.get('canon_type'), canon = metadata.get('canon'), authorid = metadata.get('author_id'), title = metadata.get('title'), updated = metadata.get('updated'), published = metadata.get('published'), language = metadata.get('lang'), genre = metadata.get('genres'), rating = metadata.get('rated'), chapters = metadata.get('num_chapters'), words = metadata.get('num_words'), reviews = metadata.get('num_reviews'), favs = metadata.get('num_favs'), follows = metadata.get('num_follows'), status = metadata.get('status'))
-c.execute(entry_command)
-print(metadata)
-connection.commit()
+while True:
+    c.execute('SELECT 1 FROM fanfiction WHERE id=? LIMIT 1', (x,))
+    exists = c.fetchone()
+
+    if exists == "None":
+
+        metadata = ffscraper.scrape_story_text(x, keep_html=False)
+
+        fulltext = ''
+        for y in range(0, len(metadata['chapters'].keys())):
+            fulltext = fulltext + str(metadata['chapters'][x+1])
+
+        fulltext = fulltext.replace("'",'"')
+
+        c.execute('''INSERT INTO fanfiction (id, canon_type, canon, authorid, title, updated, published, language, genre, rating, chapters, words, reviews, favs, follows, status, story)
+                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (metadata.get('id'), metadata.get('canon_type'), metadata.get('canon'), metadata.get('author_id'), metadata.get('title'), metadata.get('updated'), metadata.get('published'), metadata.get('lang'), " ".join(metadata.get('genres')), metadata.get('rated'), metadata.get('num_chapters'), metadata.get('num_words'), metadata.get('num_reviews'), metadata.get('num_favs'), metadata.get('num_follows'), metadata.get('status'), fulltext))
+        print('Gathered metadata for id:',int(metadata.get('id')))
+
+        connection.commit()
+        time.sleep(.5)
+    
+    else:
+        print("Story with ID",x,"already exists in database, skipping.")
+    x+=1
+
